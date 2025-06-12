e75d1de5673b9c533f46a4b49f19cf38163601f7
hydra <navidsoleymani@ymail.com>
2025-06-12 08:53:56 -0600

    Commenting within the source code for greater readability... .

--- app/db/admin.py
+++ app/db/admin.py
@@ -3,10 +3,16 @@ from sqladmin import ModelView
 from .models import URLVisit, URL
 
 
+# Admin view for the URL model
 class URLAdmin(ModelView, model=URL):
+    # Columns to display in the admin list view
     column_list = [URL.id, URL.original_url, URL.short_code]
+
+    # Fields to include in the admin form
     form_columns = [URL.original_url, URL.short_code]
 
 
+# Admin view for the URLVisit model
 class URLVisitAdmin(ModelView, model=URLVisit):
+    # Columns to display in the admin list view
     column_list = [URLVisit.id, URLVisit.url, URLVisit.timestamp]
