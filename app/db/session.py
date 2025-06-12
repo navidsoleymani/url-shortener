82507427ec1094e2a4c86fc1623130ae9f0531dd
hydra <navidsoleymani@ymail.com>
2025-06-12 03:32:17 -0600

    Update the required tool to better handle errors, add logging and parameter values to the "create_async_engine" function...

--- app/db/session.py
+++ app/db/session.py
@@ -3,32 +3,37 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
 
-from core.setting import settings
+from app.core.setting import settings
+import logging
+
+logger = logging.getLogger("uvicorn.access")
 
 engine = create_async_engine(
     settings.PG_DSN,
-    echo=True,
+    echo=settings.DB_ECHO,
+)
+
+async_session_maker = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False,
 )
 
 
-def create_async_session():
-    return sessionmaker(
-        bind=engine,
-        autocommit=False,
-        autoflush=False,
-        expire_on_commit=False,
-        class_=AsyncSession,
-    )
-
-
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async_session = create_async_session()
-    async with async_session() as session:
+    async with async_session_maker() as session:
         try:
             yield session
-        except Exception as e:
+        except SQLAlchemyError as e:
+            logger.exception("Database error")
             await session.rollback()
-            raise e
+            raise
+        except Exception:
+            await session.rollback()
+            raise
         finally:
             await session.close()
